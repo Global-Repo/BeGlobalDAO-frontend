bd0b67db864c28eece5e6d584b0baecbe098860a
gyss <gravedian@gmail.com>
2021-12-29 22:31:46 +0000

    finish GLBD Top menu

--- src/components/TopBar/OhmMenu.jsx
+++ src/components/TopBar/OhmMenu.jsx
@@ -4,9 +4,9 @@ import { NavLink } from "react-router-dom";
 import { Link, SvgIcon, Popper, Button, Paper, Typography, Divider, Box, Fade, Slide } from "@material-ui/core";
 import { ReactComponent as InfoIcon } from "../../assets/icons/info-fill.svg";
 import { ReactComponent as ArrowUpIcon } from "../../assets/icons/arrow-up.svg";
-import { ReactComponent as sOhmTokenImg } from "../../assets/tokens/token_sOHM.svg";
+import { ReactComponent as sOhmTokenImg } from "../../assets/tokens/GLBD.svg";
 import { ReactComponent as wsOhmTokenImg } from "../../assets/tokens/token_wsOHM.svg";
-import { ReactComponent as ohmTokenImg } from "../../assets/tokens/token_OHM.svg";
+import { ReactComponent as ohmTokenImg } from "../../assets/tokens/GLBD.svg";
 import { ReactComponent as t33TokenImg } from "../../assets/tokens/token_33T.svg";
 import "./ohmmenu.scss";
 import { dai, frax } from "src/helpers/AllBonds";
@@ -15,17 +15,23 @@ import Grid from "@material-ui/core/Grid";
 import OhmImg from "src/assets/tokens/token_OHM.svg";
 import SOhmImg from "src/assets/tokens/token_sOHM.svg";
 import WsOhmImg from "src/assets/tokens/token_wsOHM.svg";
+import GlbdImg from "src/assets/tokens/GLBD.svg";
 import token33tImg from "src/assets/tokens/token_33T.svg";
 import { segmentUA } from "../../helpers/userAnalyticHelpers";
 import { useSelector } from "react-redux";
 import { useWeb3Context } from "../../hooks";
 
 const addTokenToWallet = (tokenSymbol, tokenAddress, address) => async () => {
+  debugger;
   if (window.ethereum) {
     const host = window.location.origin;
     let tokenPath;
     let tokenDecimals = TOKEN_DECIMALS;
     switch (tokenSymbol) {
+      case "GBLD":
+      case "sGBLD":
+        tokenPath = GlbdImg;
+        break;
       case "OHM":
         tokenPath = OhmImg;
         break;
@@ -111,33 +117,17 @@ function OhmMenu() {
             <Fade {...TransitionProps} timeout={100}>
               <Paper className="ohm-menu" elevation={1}>
                 <Box component="div" className="buy-tokens">
-                  <Link
-                    href={`https://app.sushi.com/swap?inputCurrency=${daiAddress}&outputCurrency=${OHM_ADDRESS}`}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
+                  <Link href={`https://swap.beglobal.finance/#/swap`} target="_blank" rel="noreferrer">
                     <Button size="large" variant="contained" color="secondary" fullWidth>
                       <Typography align="left">
-                        <Trans>Buy on {new String("Sushiswap")}</Trans>
-                        <SvgIcon component={ArrowUpIcon} htmlColor="#A3A3A3" />
-                      </Typography>
-                    </Button>
-                  </Link>
-
-                  <Link
-                    href={`https://app.uniswap.org/#/swap?inputCurrency=${fraxAddress}&outputCurrency=${OHM_ADDRESS}`}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <Button size="large" variant="contained" color="secondary" fullWidth>
-                      <Typography align="left">
-                        <Trans>Buy on {new String("Uniswap")}</Trans>
+                        <Trans>Buy on BeGlobal finance</Trans>
                         <SvgIcon component={ArrowUpIcon} htmlColor="#A3A3A3" />
                       </Typography>
                     </Button>
                   </Link>
                 </Box>
 
+                {/*
                 <Box component="div" className="data-links">
                   <Divider color="secondary" className="less-margin" />
                   <Link href={`https://dune.xyz/shadow/Olympus-(OHM)`} target="_blank" rel="noreferrer">
@@ -148,6 +138,7 @@ function OhmMenu() {
                     </Button>
                   </Link>
                 </Box>
+                */}
 
                 {isEthereumAPIAvailable ? (
                   <Box className="add-tokens">
@@ -160,11 +151,11 @@ function OhmMenu() {
                         <Button
                           variant="contained"
                           color="secondary"
-                          onClick={addTokenToWallet("OHM", OHM_ADDRESS, address)}
+                          onClick={addTokenToWallet("GBLD", OHM_ADDRESS, address)}
                         >
                           <SvgIcon
                             component={ohmTokenImg}
-                            viewBox="0 0 32 32"
+                            viewBox="0 0 263 263"
                             style={{ height: "25px", width: "25px" }}
                           />
                           <Typography variant="body1">GLBD</Typography>
@@ -174,11 +165,11 @@ function OhmMenu() {
                         <Button
                           variant="contained"
                           color="secondary"
-                          onClick={addTokenToWallet("sOHM", SOHM_ADDRESS, address)}
+                          onClick={addTokenToWallet("sGBLD", SOHM_ADDRESS, address)}
                         >
                           <SvgIcon
                             component={sOhmTokenImg}
-                            viewBox="0 0 100 100"
+                            viewBox="0 0 263 263"
                             style={{ height: "25px", width: "25px" }}
                           />
                           <Typography variant="body1">sGLBD</Typography>
@@ -216,6 +207,7 @@ function OhmMenu() {
                   </Box>
                 ) : null}
 
+                {/*
                 <Divider color="secondary" />
                 <Link
                   href="https://docs.olympusdao.finance/using-the-website/unstaking_lp"
@@ -239,6 +231,7 @@ function OhmMenu() {
                     </Typography>
                   </Button>
                 </Link>
+                */}
               </Paper>
             </Fade>
           );
