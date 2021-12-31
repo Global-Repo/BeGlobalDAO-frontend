d690a12faedb9a87f6415bef1c6584f00d89bfad
Jordi Radev <>
2021-12-31 12:35:42 +0100

    Changed some constants like EPOCH_INTERVAL & BLOCK_RATE_SECONDS

--- src/constants.ts
+++ src/constants.ts
@@ -11,10 +11,10 @@ export const NETWORK_IDS = {
 };
 
 export const THE_GRAPH_URL = "https://api.thegraph.com/subgraphs/name/drondin/olympus-protocol-metrics";
-export const EPOCH_INTERVAL = 2200;
+export const EPOCH_INTERVAL = 200;
 
 // NOTE could get this from an outside source since it changes slightly over time
-export const BLOCK_RATE_SECONDS = 13.14;
+export const BLOCK_RATE_SECONDS = 3;
 
 export const TOKEN_DECIMALS = 9;
 
